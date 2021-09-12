6dc570a97ba8da894209be06ddd38393a09964b1
Felipe Dias <felipeferreiradias1@gmail.com>
2021-09-12 18:39:18 -0300

    fix lint

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,25 +4,23 @@ import PropTypes from 'prop-types';
 import TableLines from './TableLines';
 
 class Table extends Component {
-  
-
   render() {
     const { expenses } = this.props;
     return (
       <table>
         <thead>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-      </thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
         <tbody>
           {
             expenses
